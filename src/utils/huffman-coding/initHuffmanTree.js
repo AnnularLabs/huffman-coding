462817132730a5b566577e4dcf06b0f5f3a1f5e5
Agility6 <agility6@163.com>
2023-05-16 10:43:38 +0800

    feat: add letter frequency

--- src/utils/huffman-coding/initHuffmanTree.js
+++ src/utils/huffman-coding/initHuffmanTree.js
@@ -9,14 +9,43 @@ class HNode {
   }
 }
 
+
+/**
+ *  获取所有字母的频率表 
+ *  当输入 hello的时候
+ *  方案一：直接生成26个字母的huffmanTree
+ *  方案二：在用的时候去生成
+ *    - 在获取word的每个字母時需要按频率表进行添加
+ *    - 需要获取下标
+ * 
+ * 
+ */
+
 // 初始化HuffmanTree
-export default function (words) {
+export default function ([...words]) {
   const HuffmanTree = [0]
 
+  // 字母频率 from small to large
+  const LETTER_FREQUENCIES = [
+    'q', 'j', 'z', 'x', 'v', 'k',
+    'w', 'y', 'f', 'b', 'g', 'h',
+    'm', 'p', 'd', 'u', 'c', 'l',
+    's', 'n', 't', 'o', 'i', 'r',
+    'a', 'e'
+  ]
+  
   // 添加words的HNode
-  for (let i = 1; i <= words.length; i++)
-    HuffmanTree.push(new HNode(words[i-1], i))
-    // console.log(HuffmanTree)  
+  words.forEach(item => {
+    const letterWeight = LETTER_FREQUENCIES.indexOf(item)
+    const defaultWeight = 10
+    if (letterWeight === -1) {
+      // 当不是letter的时候默认权重为10
+      HuffmanTree.push(new HNode(item, defaultWeight))
+    } else {
+      HuffmanTree.push(new HNode(item, letterWeight)) 
+    }
+  });
+
   // 添加生成的HNode
   for(let i = 1; i <= words.length - 1; i++) {
     HuffmanTree.push(new HNode())
